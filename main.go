f5f10dd3b7908aad54872722baa489dc083af7a4
dirkarnez <smalldirkalex@gmail.com>
2018-11-05 12:01:20 +0800

    - update csv_to_sql_insert

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dirkarnez/my-slave/impl"
 	"log"
 )
@@ -9,7 +10,10 @@ func main() {
 	inPath := "test.txt"
 	outPath := "out.txt"
 
-	err := impl.CsvToSqlInsert(inPath, outPath)
+	err := impl.CsvToSqlInsert(inPath,  func(titles map[string]int, record []string) string {
+		return fmt.Sprintf("INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')", record[titles["xxx"]], record[titles["yyy"]], record[titles["zzz"]])
+	}, outPath)
+
 	//err := impl.ApiParamToCsField(inPath, outPath)
 	//err := impl.UniqueLines(inPath, outPath)
 	if err != nil {
